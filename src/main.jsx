fa112f82ca7c2e0133986e492e109e27506b6bd4
goho0327-droid <goho0327@gmail.com>
2025-10-24 23:18:47 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App'
+// src/main.jsx
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx'; // ← 대소문자/확장자 꼭 맞추기!
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
